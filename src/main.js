493f2817a9d1958b22eaf8d87cabaf966febfc48
Leonardo Henrique Constantino <leohconstantino@hotmail.com>
2024-06-17 13:01:26 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,23 +9,36 @@ import { showDadosSalvos } from './services/showDadosSalvos.js';
 import { showSnackbar } from './utils/showSnackbar.js';
 
 /**
- * Função para inicializar a aplicação quando o DOM estiver totalmente carregado.
+ * Função principal para inicializar a aplicação quando o DOM estiver totalmente carregado.
  * A função tenta renderizar os componentes h1, details, table e footer no elemento com o ID 'app'.
  * Em seguida, tenta mostrar os dados salvos usando a função 'showDadosSalvos'.
  * Se ocorrer um erro durante a inicialização, a função exibe uma mensagem de erro usando a função 'showSnackbar'.
  */
-document.addEventListener('DOMContentLoaded', () => {
+const main = () => {
     try {
+        // Seleciona o elemento com o ID 'app' para renderizar os componentes
         const app = document.querySelector('#app');
 
+        // Renderiza o componente h1 dentro do elemento 'app'
         renderElement(h1, true, app);
+
+        // Renderiza o componente details dentro do elemento 'app'
         renderElement(details, true, app);
+
+        // Renderiza o componente table dentro do elemento 'app'
         renderElement(table, true, app);
+
+        // Renderiza o componente footer fora do elemento 'app' (provavelmente no final da página)
         renderElement(footer, true);
 
+        // Mostra os dados salvos, se houver algum
         showDadosSalvos();
     } catch (error) {
+        // Exibe uma mensagem de erro se ocorrer um problema durante a inicialização
         showSnackbar('Erro ao carregar a aplicação!', 5000);
         console.error(error);
     }
-});
+}
+
+// Adiciona um evento ao documento para chamar a função 'main' quando o DOM estiver completamente carregado
+document.addEventListener('DOMContentLoaded', main);
